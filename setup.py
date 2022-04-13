086757feb6ab1e3851503b2a97bd2acbe09f4226
Janix4000 <janix4000@gmail.com>
2022-04-13 13:29:40 +0200

    change supported python versions

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setuptools.setup(
     license="MIT License",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
