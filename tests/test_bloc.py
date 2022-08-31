f6b85a38de3089345f897f8e7d78e5f9a10addfc
Janix4000 <janix4000@gmail.com>
2022-08-31 17:47:08 +0200

    Example test loads tests from files
    Generate `json` test files and load them using utils functions

--- tests/test_bloc.py
+++ tests/test_bloc.py
@@ -1,28 +1,37 @@
 from votepy.bloc import bloc
 from votepy.ordinal_election import OrdinalElection
+import os
 
-from .util.util import assert_approximation
+from tests.util.util import assert_approximation, load_tests, generate_tests
+
+from itertools import product
+
+alpha = 0.99
+
+
+def scoring(voting: OrdinalElection, committee: list[int]) -> float:
+    committee_size = len(committee)
+    score = 0.0
+    for ballot in voting:
+        for candidate in committee:
+            score += 1.0 if ballot.pos(candidate) < committee_size else 0.0
+    return score
 
 
 def test_bloc():
-    election = OrdinalElection([
-        [0, 1, 2, 3, 4],
-        [4, 0, 1, 3, 2],
-        [3, 0, 1, 2, 4],
-        [2, 1, 3, 4, 0],
-        [2, 1, 4, 0, 3],
-        [1, 2, 3, 4, 0]
-    ], {
-        0: 'a',
-        1: 'b',
-        2: 'c',
-        3: 'd',
-        4: 'e'
-    })
+    print(os.getcwd())
+    for voting, size_of_committee, expected_score in load_tests("tests/fixtures/bloc_medium.json"):
+        voting = OrdinalElection(voting)
+        committee = bloc(voting, size_of_committee)
+        score = scoring(voting, committee)
+        assert (score >= alpha * expected_score)
 
-    assert_approximation(
-        election,
-        lambda _e: 10,
-        10,
-        0.8
+
+if __name__ == "__main__":
+    generate_tests(
+        "tests/fixtures/bloc_medium.json",
+        filter(lambda x: x[0] + x[1] > 5 and x[0] >
+               0 and x[1] > 0, product(range(20), range(20))),
+        lambda c, v: OrdinalElection([list(range(c)) for _ in range(v)]),
+        lambda e, c: scoring(e, bloc(e, c))
     )
