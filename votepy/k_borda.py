95600795b977483f26bc70ac9016334be4a44c59
Janix4000 <janix4000@gmail.com>
2022-10-11 14:26:38 +0200

    Simplify `k-borda` implementation
    The implementation is now more like a mathematical form
    from the papers.
    Additionally, function returned tuple, instead of the list.

--- votepy/k_borda.py
+++ votepy/k_borda.py
@@ -23,21 +23,22 @@ def k_borda(voting: Union[OrdinalElection, list[int]], size_of_committee: int, n
         voting = OrdinalElection(voting)
 
     n = voting.ballot_size
-    results = [0] * n
     if size_of_committee > n or size_of_committee <= 0:
         raise ValueError(
             f"Size of committee needs to be from the range 1 to the number of all candidates.")
     if number_of_scored_candidates > n or number_of_scored_candidates <= 0:
         raise ValueError(
             f"Number of candidates scored in k-borda needs to be from range 1 to the number of all candidates.")
+
+    candidates_scores = [0] * n
+    m = number_of_scored_candidates
+
     for vote in voting:
-        m = number_of_scored_candidates
-        for candidate in vote[:number_of_scored_candidates]:
-            results[candidate] += m
-            m -= 1
+        for i, candidate in enumerate(vote[:number_of_scored_candidates]):
+            candidates_scores[candidate] += m - i
     committee, _ = zip(
-        *sorted(enumerate(results), reverse=True, key=lambda t: t[1]))
-    return committee[:size_of_committee]
+        *sorted(enumerate(candidates_scores), reverse=True, key=lambda t: t[1]))
+    return list(committee[:size_of_committee])
 
 
 if __name__ == '__main__':
@@ -45,4 +46,4 @@ if __name__ == '__main__':
         [0, 1, 2, 3],
         [3, 2, 1, 0],
         [2, 1, 3, 0]
-    ], 2, 3))
+    ], 3, 3))
