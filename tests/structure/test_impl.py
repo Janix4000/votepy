88ff403fdb2edbae0066c9a556144d3139f45c93
Janix4000 <janix4000@gmail.com>
2022-10-17 22:22:17 +0200

    Test integration between `algo` and `rule`
    Test `impl` decorator.
    Additional checks and helpful exceptions in the decorators

--- tests/structure/test_impl.py
+++ tests/structure/test_impl.py
@@ -1,1 +1,105 @@
-from votepy.testing.structure.structure import rule, impl, implementations
+from votepy.testing.structure.structure import rule, impl, algo, implementations, get_implementation
+
+import pytest
+
+from abc import ABC, abstractmethod
+
+
+class MockBaseAlgorithm(ABC):
+    def __init__(self):
+        self.__prepared = False
+
+    @abstractmethod
+    def _solve(self, x: int, y: int) -> int:
+        pass
+
+    def solve(self, x: int, y: int) -> int:
+        return self._solve(x, y)
+
+    def prepare(self):
+        self.__prepared = True
+
+
+@algo(name="slow")
+class Slow(MockBaseAlgorithm):
+    def __init__(self, max_iterations: int) -> None:
+        super().__init__()
+        self.max_iterations = max_iterations
+
+    def _solve(self, x: int, y: int):
+        res = 0
+        for _ in range(max(x, self.max_iterations)):
+            res += 1
+        for _ in range(max(y, self.max_iterations)):
+            res += 1
+        return res
+
+
+@algo(name="fast")
+class Fast(MockBaseAlgorithm):
+    def _solve(self, x: int, y: int) -> int:
+        print(self.prompt)
+        return x + y
+
+    def prepare(self, custom_prompt: str) -> None:
+        self.prompt = custom_prompt
+        super().prepare()
+
+
+@rule()
+def add(x: int, y: int, algorithm: MockBaseAlgorithm = Fast()) -> int:
+    """# Summary
+    Add two numbers
+
+    ## Args:
+        `x` (int): First number to add
+        `y` (int): Second number to add
+
+    ## Returns:
+        -> int: Sum of two numbers
+
+    ## Examples
+    >>> add(1, 2)
+    specific prompt for add_fast
+    3
+    """
+    implementation = get_implementation(add, algorithm)
+    return implementation(x, y, algorithm)
+
+
+@impl(add, Slow)
+def add_slow(x, y, algorithm: Slow) -> int:
+    return algorithm.solve(x, y)
+
+
+@impl(add, Fast)
+def add_fast(x, y, algorithm: Fast = Fast()) -> int:
+    algorithm.prepare("specific prompt for add_fast")
+    return algorithm.solve(x, y)
+
+
+def test_implementations_registered():
+    assert 'add' in implementations
+    add_imps = implementations['add']
+    assert 'slow' in add_imps
+    assert 'fast' in add_imps
+
+
+def test_get_existing_implementations():
+    assert get_implementation(add, Slow)
+    assert get_implementation(add, "slow")
+    assert get_implementation("add", Slow)
+    assert get_implementation("add", "slow")
+
+    assert get_implementation(add, Fast)
+    assert get_implementation(add, "fast")
+    assert get_implementation("add", Fast)
+    assert get_implementation("add", "fast")
+
+
+def test_get_non_existing_implementations():
+    with pytest.raises(ValueError):
+        get_implementation("sub", Slow)
+
+    with pytest.raises(ValueError):
+        get_implementation(add, "int")
--- votepy/testing/structure/structure.py
+++ votepy/testing/structure/structure.py
@@ -9,23 +9,6 @@ algorithms: dict[str, BaseAlgorithm] = dict()
 rules: dict[str, Callable] = dict()
 
 
-def impl(rule: Callable, algorithm: BaseAlgorithm):
-    def inner(fun):
-        rule_name = rule if isinstance(rule, str) else rule.__name__
-        algorithm_name = algorithm if isinstance(
-            algorithm, str) else algorithm.name
-        implementations[rule_name][algorithm_name] = fun
-        return fun
-    return inner
-
-
-def get_implementation(rule: Callable, algorithm: BaseAlgorithm):
-    rule_name = rule if isinstance(rule, str) else rule.__name__
-    algorithm_name = algorithm if isinstance(
-        algorithm, str) else algorithm.name
-    return implementations[rule_name][algorithm_name]
-
-
 def algo(name: str):
     def inner(Algorithm):
         Algorithm.name = name
@@ -45,6 +28,39 @@ def rule(name: str = None, auto_imp: bool = False):
 
         rule_name = name if name is not None else rule.__name__
         rules[rule_name] = wrapper
+        implementations[rule_name] = {}
 
         return wrapper
     return actual_decorator
+
+
+def impl(rule: Callable, algorithm: BaseAlgorithm):
+    def actual_decorator(implementation):
+
+        @wraps(implementation)
+        def wrapper(*args, **kwargs):
+            return implementation(*args, **kwargs)
+
+        rule_name = rule if isinstance(rule, str) else rule.__name__
+        algorithm_name = algorithm if isinstance(
+            algorithm, str) else algorithm.name
+        implementations[rule_name][algorithm_name] = wrapper
+
+        return wrapper
+    return actual_decorator
+
+
+def get_implementation(rule: Callable, algorithm: BaseAlgorithm) -> Callable:
+    rule_name = rule if isinstance(rule, str) else rule.__name__
+    algorithm_name = algorithm if isinstance(
+        algorithm, str) else algorithm.name
+
+    if rule_name not in implementations:
+        raise ValueError(f"Rule {rule_name} has not been registered. Remember to register the main rule function with `@rule()` decorator.")
+
+    rule_implementations = implementations[rule_name]
+
+    if algorithm_name not in rule_implementations:
+        raise ValueError(f"Algorithm {algorithm_name} has not been adapted to {rule_name}. Remember to register algorithm use with `@impl()` decorator.")
+
+    return rule_implementations[algorithm_name]
