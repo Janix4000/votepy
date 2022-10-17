d2106b9d5d90c0b274c27d0d3787a6e9f9492263
Janix4000 <janix4000@gmail.com>
2022-10-17 23:49:33 +0200

    Document structure code

--- votepy/testing/structure/structure.py
+++ votepy/testing/structure/structure.py
@@ -11,6 +11,24 @@ rules: dict[str, Callable] = dict()
 
 
 def algo(name: str):
+    """# Summary
+    Decorator used to register voting solving algorithm classes.
+    Voting algorithm must derive from the `BaseAlgorithm`.
+
+    ## Args:
+        `name` (str): Identification name of the algorithm.
+
+    ## Examples
+    >>> from votepy.testing.algorithms.base_algorithm import BaseAlgorithm
+    >>> 
+    >>> @algo(name='name')
+    ... class Algo(BaseAlgorithm):
+    ...     def _solve():
+    ...         pass
+    >>>
+        get_algorithm('name') == Algo()
+    True
+    """
     def inner(Algorithm):
         Algorithm.name = name
         algorithms[name] = Algorithm
@@ -19,6 +37,35 @@ def algo(name: str):
 
 
 def get_algorithm(algorithm: Union[str, BaseAlgorithm], *args, **kwargs) -> BaseAlgorithm:
+    """# Summary
+    Returns initialized algorithm object, registered with `@algo(name=)` decorator.
+
+    ## Args:
+        `algorithm` (str | BaseAlgorithm): Name identification of the algorithm, class deriving from `BaseAlgorithm` or already initialized algorithm. 
+
+    ## Raises:
+        `ValueError`: Algorithm has not been registered.
+        `TypeError`: Object did not derived from the `BaseAlgorithm`.
+
+    ## Returns:
+        -> BaseAlgorithm: Ready to use algorithm object. If the given algorithm is already an object, it is passed by.
+
+    ## Examples
+    >>> from votepy.testing.algorithms.base_algorithm import BaseAlgorithm
+    >>> 
+    >>> @algo(name='name')
+    ... class Algo(BaseAlgorithm):
+    ...     def _solve():
+    ...         pass
+    >>>
+        get_algorithm('name') == Algo()
+    True
+        get_algorithm(Algo) == Algo()
+    True
+        get_algorithm(Algo()) == Algo()
+    True
+    """
+
     if isinstance(algorithm, str):
         if algorithm not in algorithms:
             raise ValueError(
@@ -35,14 +82,24 @@ def get_algorithm(algorithm: Union[str, BaseAlgorithm], *args, **kwargs) -> Base
         raise TypeError(f"Algorithm {algorithm} must derive from the `BaseAlgorithm`")
 
 
-def rule(name: str = None, auto_imp: bool = False):
+def rule(name: str = None):
+    """# Summary
+    Decorator used to register a voting rule function.
+
+    ### IT MUST BE USED WITH PARENTHESES, EVEN WITHOUT SPECIFYING `NAME` ARGUMENT.
+
+    ## Args:
+        `name` (str, optional): Identification name of the voting rule. Defaults to the name of the function.
+
+    ## Examples
+    >>> @rule()
+    ... def some_rule():
+    ...     pass
+    """
     def actual_decorator(rule: Callable):
         @wraps(rule)
         def wrapper(*args, **kwargs):
-            if auto_imp:
-                raise NotImplementedError()
-            else:
-                return rule(*args, **kwargs)
+            return rule(*args, **kwargs)
 
         rule_name = name if name is not None else rule.__name__
         rules[rule_name] = wrapper
@@ -52,7 +109,35 @@ def rule(name: str = None, auto_imp: bool = False):
     return actual_decorator
 
 
-def impl(rule: Callable, algorithm: BaseAlgorithm):
+def impl(rule: Union[Callable, str], algorithm: BaseAlgorithm):
+    """# Summary
+    Decorator used to register specific implementation of the voting rule solving function, using algorithm. 
+
+    Implementation function must have `algorithm` argument.
+
+    ## Args:
+        `rule` (Callable): Rule function or its identification name.
+        `algorithm` (BaseAlgorithm): Algorithm class or its identification name.
+
+    ## Examples
+    >>> from votepy.testing.algorithms.base_algorithm import BaseAlgorithm
+    >>> 
+    >>> @algo(name='name')
+    ... class Algo(BaseAlgorithm):
+    ...     def _solve():
+    ...         pass
+    >>>
+    >>> @rule()
+    ... def some_rule():
+    ...     pass
+    >>>
+    >>> @impl(some_rule, Algo)
+    ... def some_rule_algo(algorithm):
+    ...     pass
+    >>> 
+    >>> get_implementation(some_rule, Algo) == some_rule_algo
+    True
+    """
     def actual_decorator(implementation):
 
         @wraps(implementation)
