d934b62b3ab0ca2895f85130af10b802e6b16279
Janix4000 <janix4000@gmail.com>
2022-10-18 14:20:11 +0200

    Improve solve documentation
    Add docstring and explicit `voting` and `size` arguments

--- votepy/testing/solve.py
+++ votepy/testing/solve.py
@@ -1,9 +1,27 @@
 from typing import Callable, Union
+from votepy.ordinal_election import OrdinalElection
 from votepy.testing.algorithms.base_algorithm import BaseAlgorithm
 from votepy.testing.structure.structure import get_algorithm, get_implementation
 
 
-def solve(rule: Union[Callable, str], *rule_args, algorithm: BaseAlgorithm, **rule_kwargs):
+def solve(rule: Union[Callable, str], voting: Union[OrdinalElection, list[int]], size_of_committee: int, *rule_args, algorithm: BaseAlgorithm, **rule_kwargs) -> list[int]:
+    """# Summary
+    Calculates committee of the given size, based on the voting rule, using algorithm. 
+
+    ## Args:
+        `rule` (Callable | str): Voting rule function or its identification name
+        `voting` (Union[OrdinalElection, list[int]]): Voting for which the function calculates the committee
+        `size_of_committee` (int): Size of the committee
+        `algorithm` (BaseAlgorithm): Voting solving algorithm or its identification name.
+        `*rule_args`: Additional voting functions' arguments
+        `*rule_kwargs`: Additional voting functions' positional arguments
+
+    ## Returns:
+        -> list[int]: List of chosen candidates
+
+    ## Examples
+
+    """
     implementation = get_implementation(rule, algorithm)
     algorithm = get_algorithm(algorithm)
-    return implementation(*rule_args, algorithm=algorithm, **rule_kwargs)
+    return list(implementation(voting, size_of_committee, *rule_args, algorithm=algorithm, **rule_kwargs))
