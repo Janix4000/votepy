f8b74cd33836a12af9072008520db23977c59889
Janix4000 <janix4000@gmail.com>
2022-10-23 17:04:23 +0200

    Fix Chamberlin-Courant algorithm
    Previous version had wrong scoring function. New one is based on
    the definition in the papers.
    
    Add docstrings with examples

--- votepy/rules/chamberlin_courant.py
+++ votepy/rules/chamberlin_courant.py
@@ -1,6 +1,8 @@
 from votepy.ordinal_election import OrdinalElection
 
-from votepy.structure.structure import rule, get_implementation, impl
+from votepy.structure.structure import rule, impl
+from votepy.solve import solve
+
 from votepy.algorithms.base_algorithm import BaseAlgorithm
 from votepy.algorithms.generic_brute_force import BruteForce
 from votepy.algorithms.generic_greedy import Greedy
@@ -9,51 +11,69 @@ from typing import Union, Iterable
 
 
 @rule()
-def chamberlin_courant(voting: Union[OrdinalElection, list[int]], size_of_committee: int, number_of_scored_candidates: int, algorithm: BaseAlgorithm) -> list[int]:
-    """Chamberlin-courant rule
-    Args:
-        `voting` (`Union[OrdinalElection, list[int]]`): Voting for which the function calculates the committee
+def chamberlin_courant(voting: Union[OrdinalElection, list[int]], size_of_committee: int, algorithm: BaseAlgorithm) -> list[int]:
+    """# Summary
+    Chamberlin-Courant rule
+    ## Args:
+        `voting` (`OrdinalElection | list[int]`): Voting for which the function calculates the committee
         `size_of_committee` (`int`): Size of the committee
         `number_of_scored_candidates` (`int`): Number of scored candidates using k-borda rule
 
-    Returns:
+    ## Returns:
         `list[int]`: List of chosen candidates
+
+    ## Examples
+    >>> election = [
+    ...     [0, 1, 2, 3, 4],
+    ...     [4, 0, 1, 3, 2],
+    ...     [3, 0, 1, 2, 4],
+    ...     [2, 1, 3, 4, 0],
+    ...     [2, 1, 4, 0, 3],
+    ...     [1, 2, 3, 4, 0]
+    ... ]
+    >>> chamberlin_courant(election, 2, algorithm='brute_force')
+    [0, 2]
+    >>> chamberlin_courant(election, 2, algorithm='greedy')
+    [1, 0]
     """
-    implementation = get_implementation(chamberlin_courant, algorithm)
-    implementation(voting, size_of_committee,
-                   number_of_scored_candidates, algorithm)
+    return solve(chamberlin_courant, voting, size_of_committee, algorithm=algorithm)
+
+
+def scoring_function(committee: Iterable[int], voting: OrdinalElection) -> float:
+    """# Summary
+    Scoring function for Chamberlin-Courant rule. Calculates score for the given committee, under CC rule and given voting.ballot_size
+
+    ## Args:
+        `committee` (Iterable[int]): Chosen committee
+        `voting` (OrdinalElection): Voting
+
+    ## Returns:
+        -> float: Score of the committee
+
+    ## Examples
+    """
+    committee = set(committee)
+    score = 0
+    m = voting.ballot_size
+    for voter in voting:
+        score += max(m - voter.pos(candidate) for candidate in committee)
+    return score
 
 
 @impl(chamberlin_courant, BruteForce)
-def chamberlin_courant_brute_force(voting, size_of_committee, number_of_scored_candidates, brute_force: BruteForce = BruteForce()) -> list[int]:
-    def scoring_function(committee: Iterable[int], voting: OrdinalElection):
-        committee = set(committee)
-        score = 0
-        for vote in voting:
-            for i, candidate in enumerate(vote):
-                if candidate in committee:
-                    score += max(number_of_scored_candidates - i, 0)
-                    break
-        return score
-
-    brute_force.prepare(scoring_function)
-    return brute_force.solve(voting, size_of_committee)
+def chamberlin_courant_brute_force(voting, size_of_committee, algorithm: BruteForce = BruteForce()) -> list[int]:
+    algorithm.prepare(scoring_function)
+    return algorithm.solve(voting, size_of_committee)
 
 
 @impl(chamberlin_courant, Greedy)
-def chamberlin_courant_greedy(voting, size_of_committee, number_of_scored_candidates, greedy: Greedy = Greedy()) -> list[int]:
-    def scoring_function(committee: Iterable[int], voting: OrdinalElection, candidate: int):
+def chamberlin_courant_greedy(voting, size_of_committee, algorithm: Greedy = Greedy()) -> list[int]:
+    def greedy_scoring_function(committee: Iterable[int], voting: OrdinalElection, candidate: int):
         committee = set(committee) | {candidate}
-        score = 0
-        for vote in voting:
-            for i, candidate in enumerate(vote):
-                if candidate in committee:
-                    score += max(number_of_scored_candidates - i, 0)
-                    break
-        return score
+        return scoring_function(committee, voting)
 
-    greedy.prepare(scoring_function)
-    return greedy.solve(voting, size_of_committee)
+    algorithm.prepare(greedy_scoring_function)
+    return algorithm.solve(voting, size_of_committee)
 
 
 if __name__ == '__main__':
@@ -76,7 +96,6 @@ if __name__ == '__main__':
         chamberlin_courant(
             election,
             2,
-            5,
             algorithm='brute_force'
         )
     )
@@ -84,7 +103,6 @@ if __name__ == '__main__':
         chamberlin_courant(
             election,
             2,
-            5,
             algorithm=Greedy()
         )
     )
