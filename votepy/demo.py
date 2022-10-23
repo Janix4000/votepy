b914ef252218bf4fa349dcffc7f2595246d01166
Mateusz <msluszniak@student.agh.edu.pl>
2022-10-23 21:11:36 +0200

    Add visualization

---
+++ votepy/demo.py
@@ -0,0 +1,20 @@
+from votepy.visualization import data_to_voting, Visualizator, Generator
+from votepy.rules.k_borda import k_borda
+from votepy.bloc import bloc
+from votepy.sntv import sntv
+
+if __name__ == "__main__":
+
+    generator = Generator(num_sampling=1000)
+    data1 = generator.uniform_rectangle(50, 50, a=3, b=3)
+    data2 = generator.normal(50, 50)
+    data3 = generator.two_overlapping_circles(50, 50)
+
+    for idx, (name, data) in enumerate(
+            [("Uniform Rectangle", data1), ("Normal Distribution", data2), ("Two Overlaping Circles", data3)]):
+        for algo in [k_borda, bloc, sntv]:
+            results = data_to_voting(algo, data, size_of_committee=10)
+            vis = Visualizator(results, data)
+            vis.visualize_results(save_to_file=True,
+                                  name=f"../votepy/rules_distributions/data_{idx}_{algo.__name__}.png",
+                                  title=f"{name} using {algo.__name__}")
