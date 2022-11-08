8a102b9086aeb36f796cb4c55e823f1a9a37ff3f
Janix4000 <janix4000@gmail.com>
2022-11-08 12:28:55 +0100

    Document listings

--- votepy/structure/listing.py
+++ votepy/structure/listing.py
@@ -1,9 +1,19 @@
-from typing import Callable, Union
+from typing import Callable, Type, Union
 import votepy.structure.structure as structure
 from votepy.algorithms.base_algorithm import BaseAlgorithm
 
 
-def algorithms(rule: Union[str, Callable] = None) -> dict[str, BaseAlgorithm]:
+def algorithms(rule: Union[str, Callable] = None) -> dict[str, Type[BaseAlgorithm]]:
+    """# Summary
+    Returns a dict name->algorithm_class with all of the implemented voting solvers or only implemented for the given rule.
+    ## Args:
+        `rule` (Union[str, Callable], optional): If given, returns algorithms implemented for the given rule. Defaults to `None`.
+
+    ## Returns:
+        -> dict[str, Type[BaseAlgorithm]]: name->algorithm_class
+
+    ## Examples
+    """
     if rule is not None:
         if not isinstance(rule, str):
             rule = rule.__name__
@@ -16,4 +26,12 @@ def algorithms(rule: Union[str, Callable] = None) -> dict[str, BaseAlgorithm]:
 
 
 def rules() -> dict[str, Callable]:
+    """# Summary
+    Returns a dict name->voting_rule with all of the implemented voting rules.
+
+    ## Returns:
+        -> dict[str, Callable]: name->voting_rule
+
+    ## Examples
+    """
     return structure.rules
