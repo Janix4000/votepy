8b68734dfb9f353adda875418c7b095459132ebf
Janix4000 <janix4000@gmail.com>
2022-11-12 15:01:25 +0100

    Import sntv into main scope

--- votepy/rules/__init__.py
+++ votepy/rules/__init__.py
@@ -1,3 +1,4 @@
 from votepy.rules.chamberlin_courant import chamberlin_courant
 from votepy.rules.greedy_monroe import greedy_monroe
 from votepy.rules.k_borda import k_borda
+from votepy.rules.sntv import sntv
