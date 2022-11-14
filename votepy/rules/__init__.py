0cac521d763d71e318154de75f7a14f0325ec087
Janix4000 <janix4000@gmail.com>
2022-11-14 21:49:32 +0100

    Greedy Monroe

--- votepy/rules/__init__.py
+++ votepy/rules/__init__.py
@@ -1,5 +1,5 @@
 from votepy.rules.chamberlin_courant_impl import chamberlin_courant
-from votepy.rules.greedy_monroe import greedy_monroe
+from votepy.rules.greedy_monroe_impl import greedy_monroe
 from votepy.rules.k_borda import k_borda
 from votepy.rules.sntv import sntv
 from votepy.rules.bloc_impl import bloc
