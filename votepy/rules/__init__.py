700de3a68925c3bd1aa70d25a4fb72d48490b54c
Janix4000 <janix4000@gmail.com>
2022-11-14 21:37:59 +0100

    Add owa rules to main scope

--- votepy/rules/__init__.py
+++ votepy/rules/__init__.py
@@ -3,3 +3,4 @@ from votepy.rules.greedy_monroe import greedy_monroe
 from votepy.rules.k_borda import k_borda
 from votepy.rules.sntv import sntv
 from votepy.rules.bloc import bloc
+import votepy.rules.owa
