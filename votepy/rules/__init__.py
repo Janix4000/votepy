00fdb68eeb1490c48877ec621f48fbc82f3886a7
Janix4000 <janix4000@gmail.com>
2022-11-16 10:14:28 +0100

    `gm_impl.py` -> `gm_rule.py`

--- votepy/rules/__init__.py
+++ votepy/rules/__init__.py
@@ -1,5 +1,5 @@
 from votepy.rules.chamberlin_courant_rule import chamberlin_courant
-from votepy.rules.greedy_monroe_impl import greedy_monroe
+from votepy.rules.greedy_monroe_rule import greedy_monroe
 from votepy.rules.k_borda_impl import k_borda
 from votepy.rules.sntv_impl import sntv
 from votepy.rules.bloc_rule import bloc
