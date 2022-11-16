4d037efb4983b3a2ced1061dbaa4f914684ee8d1
Janix4000 <janix4000@gmail.com>
2022-11-16 10:13:58 +0100

    `cc_impl.py` -> `cc_rule.py`

--- votepy/rules/__init__.py
+++ votepy/rules/__init__.py
@@ -1,4 +1,4 @@
-from votepy.rules.chamberlin_courant_impl import chamberlin_courant
+from votepy.rules.chamberlin_courant_rule import chamberlin_courant
 from votepy.rules.greedy_monroe_impl import greedy_monroe
 from votepy.rules.k_borda_impl import k_borda
 from votepy.rules.sntv_impl import sntv
--- votepy/visualization.py
+++ votepy/visualization.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy import pi
 from functools import cmp_to_key
 
-from votepy.rules.chamberlin_courant_impl import chamberlin_courant_greedy
+from votepy.rules.chamberlin_courant_rule import chamberlin_courant_greedy
 from votepy.rules.k_borda_impl import k_borda
 from votepy.rules.bloc_rule import bloc
 from votepy.rules.sntv_impl import sntv
