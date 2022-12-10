3cfc331d19deae4bc67b3c01cf753f9a54e4ccbd
Janix4000 <janix4000@gmail.com>
2022-12-10 19:54:36 +0100

    Fix wrong path in tests

--- tests/preflib/test_load.py
+++ tests/preflib/test_load.py
@@ -1,5 +1,8 @@
+import os
 from votepy import preflib
 from votepy.ordinal_election import OrdinalElection
+
+dir_path = os.path.join('tests', 'fixtures', 'preflib')
 
 
 def test_load_no_dups():
@@ -8,7 +11,7 @@ def test_load_no_dups():
         [3, 2, 1, 0],
         [2, 1, 3, 0]
     ]
-    assert preflib.load('./data/voting_no_dups.txt') == expected
+    assert preflib.load(os.path.join(dir_path, 'voting_no_dups.txt')) == expected
 
 
 def test_load_with_dups():
@@ -20,7 +23,7 @@ def test_load_with_dups():
         [2, 1, 3, 0],
         [2, 1, 3, 0]
     ]
-    assert preflib.load('./data/voting_with_dups.txt') == expected
+    assert preflib.load(os.path.join(dir_path, 'voting_with_dups.txt')) == expected
 
 
 def test_load_with_names():
@@ -32,4 +35,5 @@ def test_load_with_names():
         ],
         mapping={0: 'A', 1: 'B', 2: 'C'}
     )
-    assert preflib.load('./data/voting_with_names.txt') == expected
+    assert os.getcwd() == ""
+    assert preflib.load(os.path.join(dir_path, 'voting_with_names.txt')) == expected
