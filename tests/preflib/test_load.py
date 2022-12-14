e2ecea3fdc87050f7f42a19bb9202026c42ae161
Janix4000 <janix4000@gmail.com>
2022-12-14 17:42:01 +0100

    Test curly brackets

--- tests/preflib/test_load.py
+++ tests/preflib/test_load.py
@@ -36,3 +36,12 @@ def test_load_with_names():
         mapping={0: 'A', 1: 'B', 2: 'C'}
     )
     assert preflib.load(os.path.join(dir_path, 'voting_with_names.txt')) == expected
+
+
+def test_load_with_curly_brackets():
+    expected = [
+        [0, 1, 2, 3],
+        [3, 2, 1, 0],
+        [2, 1, 3, 0]
+    ]
+    assert preflib.load(os.path.join(dir_path, 'voting_with_curly_brackets.txt')) == expected
